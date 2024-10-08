d2e51b847e2a543ea75a109045f16529109249ef
Philipp Mundhenk <philipp@mundhenk.org>
2024-10-08 21:44:42 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM alpine:3.6
+
+RUN apk add --update --no-cache docker
+
+COPY clean.sh /
+
+CMD ["/bin/bash", "/clean.sh"]
---
+++ clean.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+interval_s="${INTERVAL_S:-86400}"
+
+while true
+do
+	echo "sleeping for $interval_s seconds"
+	sleep $interval_s
+	OPTIONS=""
+	if [[ $REMOVE_ALL == "true" ]]; then
+		OPTIONS="$OPTIONS --all"
+	fi
+	if [[ $REMOVE_VOLUMES == "true" ]]; then
+		OPTIONS="$OPTIONS --volumes"
+	fi
+	docker system prune --force $OPTIONS
+done
+
